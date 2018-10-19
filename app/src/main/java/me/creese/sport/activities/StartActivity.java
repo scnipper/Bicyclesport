4b8d9f65995ae3ef8f821ee37806e2936862675a
scnipper <scnipper@creese.me>
2018-10-19 13:04:03 +0300

    add routes

--- app/src/main/java/me/creese/sport/activities/StartActivity.java
+++ app/src/main/java/me/creese/sport/activities/StartActivity.java
@@ -1,20 +1,27 @@
 package me.creese.sport.activities;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.maps.MapView;
 
-import me.creese.sport.map.MapWork;
 import me.creese.sport.R;
+import me.creese.sport.map.MapWork;
 
 public class StartActivity extends AppCompatActivity {
 
     private static final String TAG = StartActivity.class.getSimpleName();
     private MapView map;
+    private MapWork mapWork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +30,82 @@ public class StartActivity extends AppCompatActivity {
 
         map = findViewById(R.id.route_map);
         map.onCreate(savedInstanceState);
+        mapWork = new MapWork(this);
 
-        map.getMapAsync(new MapWork(this));
+        map.getMapAsync(mapWork);
     }
 
+    /**
+     * Кнопка сохранения маршрута
+     *
+     * @param v
+     */
+    public void saveRoute(View v) {
+
+        final Dialog dialog = new Dialog(this);
+
+        dialog.setContentView(R.layout.dialog_enter_name);
+
+
+        dialog.show();
+        dialog.findViewById(R.id.button_ok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text = dialog.findViewById(R.id.name_route);
+                if(!text.getText().toString().equals("")) {
+                    mapWork.getCurrentRoute().saveRoute(text.getText().toString());
+                    mapWork.getGoogleMap().clear();
+                    mapWork.clearRoute();
+                    mapWork.showStartPosition();
+
+                    findViewById(R.id.save_route_btn).setVisibility(View.GONE);
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        dialog.findViewById(R.id.button_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+
+    }
+
+    /**
+     * Кнопка старта движения
+     *
+     * @param v
+     */
+    public void playSport(View v) {
+        ImageButton button = (ImageButton) v;
+
+
+        button.setImageResource(R.drawable.baseline_stop_black_36);
+    }
+
+    /**
+     * Кнопка открытия бокового меню
+     *
+     * @param v
+     */
     public void openDrawer(View v) {
         DrawerLayout drawerLayout = findViewById(R.id.drawer);
 
         drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    /**
+     * Кнопка начала создания маршрута
+     *
+     * @param v
+     */
+    public void startMakeRoute(View v) {
+       ImageButton button = findViewById(R.id.save_route_btn);
+        button.setVisibility(View.VISIBLE);
+        mapWork.makeRoute();
     }
 
     @Override
--- app/src/main/java/me/creese/sport/map/MapWork.java
+++ app/src/main/java/me/creese/sport/map/MapWork.java
@@ -8,44 +8,108 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.maps.android.SphericalUtil;
+import com.google.android.gms.maps.model.Polyline;
+
+import java.util.ArrayList;
 
 
-public class MapWork implements OnMapReadyCallback,GpsListener,GoogleMap.OnMapClickListener {
+public class MapWork implements OnMapReadyCallback, GpsListener, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerDragListener {
     private static final String TAG = MapWork.class.getSimpleName();
     private final Gps gps;
     private final Context context;
+    private final ArrayList<LatLng> poly;
+    private Route currentRoute;
     private GoogleMap googleMap;
+    private boolean isRouteMode;
+    private Polyline lastLine;
 
     public MapWork(Context context) {
         this.context = context;
         gps = new Gps(context);
+        poly = new ArrayList<>();
+        currentRoute = new Route(context);
+    }
+
+    /**
+     * Функция создания маршрута
+     */
+    public void makeRoute() {
+        googleMap.clear();
+
+        isRouteMode = true;
     }
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
 
         this.googleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
         googleMap.setOnMapClickListener(this);
+        googleMap.setOnMarkerDragListener(this);
+        showStartPosition();
+
+
+    }
+
+    public void showStartPosition() {
         gps.getStartPosition(this);
+    }
 
+    public void clearRoute() {
+        currentRoute = new Route(context);
+    }
 
+    public GoogleMap getGoogleMap() {
+        return googleMap;
+    }
+
+    public Gps getGps() {
+        return gps;
+    }
+
+    public Route getCurrentRoute() {
+        return currentRoute;
     }
 
     @Override
     public void whenFindStartPos(LatLng pos) {
         googleMap.addMarker(new MarkerOptions()
                 .position(pos)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pos,17));
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pos, 17));
 
     }
 
     @Override
     public void onMapClick(LatLng latLng) {
-        Log.w(TAG, "onMapClick: "+latLng );
+        if (isRouteMode) {
+            googleMap.addMarker(currentRoute.addPoint(latLng));
+            lastLine = googleMap.addPolyline(currentRoute.getLine());
+        }
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        currentRoute.update(marker.getPosition());
+
+
+        lastLine.remove();
+        lastLine = googleMap.addPolyline(currentRoute.getLine());
+
+        Log.w(TAG, "onMarkerDrag: "+ marker.getPosition());
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
     }
 }
