eaf16829aecf1f8746d3e6d593d6f36152ed9727
scnipper <scnipper@creese.me>
2019-01-05 19:47:40 +0300

    add chooiser dialog library

--- app/src/main/java/me/creese/sport/util/chartformat/AxisFormat.java
+++ app/src/main/java/me/creese/sport/util/chartformat/AxisFormat.java
@@ -12,13 +12,6 @@ public class AxisFormat implements IAxisValueFormatter {
 
     private final TypeAxis typeAxis;
 
-    public enum TypeAxis {
-        DISTANCE,
-        TIME,
-        CALORIES,
-        SPEED
-    }
-
     public AxisFormat(TypeAxis typeAxis) {
         this.typeAxis = typeAxis;
     }
@@ -32,19 +25,23 @@ public class AxisFormat implements IAxisValueFormatter {
                 valueReturn = Route.makeDistance(value);
                 break;
             case TIME:
-                valueReturn = UpdateInfo.formatTime((long) (value*0.6f));
+                valueReturn = UpdateInfo.formatTime((long) (value * 0.6f));
                 break;
             case CALORIES:
-                valueReturn = (int)value+" cal";
+                valueReturn = (int) value + " cal";
                 break;
             case SPEED:
-                if(AppSettings.UNIT_SYSTEM.equals(AppSettings.UnitsSystem.METRIC))
-                valueReturn = (int) value+" км/ч";
+                if (AppSettings.UNIT_SYSTEM.equals(AppSettings.UnitsSystem.METRIC))
+                    valueReturn = (int) value + " км/ч";
                 if (AppSettings.UNIT_SYSTEM.equals(AppSettings.UnitsSystem.IMPERIAL))
-                    valueReturn = (int) (value * AppSettings.IMP_COEF* 1000)+" миль/ч";
+                    valueReturn = (int) (value * AppSettings.IMP_COEF * 1000) + " миль/ч";
                 break;
         }
 
         return valueReturn;
+    }
+
+    public enum TypeAxis {
+        DISTANCE, TIME, CALORIES, SPEED
     }
 }
